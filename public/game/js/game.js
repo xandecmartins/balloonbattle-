1d3c577c4faa05d289143bd6f610adf1b9b06b4c
alexandre <alexandre.martins@skipthedishes.ca>
2019-12-19 23:05:20 -0600

    Code improvement, bug fix on bg, finish feature of end local game to wait final result

--- public/game/js/game.js
+++ public/game/js/game.js
@@ -1,3 +1,5 @@
+//<-------------------------- Config for Firebase and Musics-------------------------->
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyDj_qwUrYCUsEstUJE9wo2ZpuLD_1LGjVY',
@@ -31,6 +33,8 @@ const soundMap = {
   'background_music': new Audio('sounds/top-gear-soundtrack-track-1.mp3'),
 };
 
+//<-------------------------- Util Functions -------------------------->
+
 //src: https://gist.github.com/jed/982883
 function createUUID() {
   return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
@@ -50,43 +54,75 @@ function generateRandomXPos(limit) {
   return Math.floor(Math.random() * limit);
 }
 
+//<-------------------------- Constants and HTML Reference -------------------------->
+
 const balloonInitialWidth = 40;
 const balloonInitialHeight = 53;
 const updateTime = 50;
+let canvasHeight = 450;
+
+const canvasElement = document.getElementById('canvas');
+const startPlayElement = document.getElementById('start-btn');
+const nameBoxElem = document.getElementById('name');
+const scoreLabelElem = document.getElementById('score-count');
+const scoreTextElem = document.getElementById('score-text');
+const nameLabelElem = document.getElementById('name-show');
+const idLabelElem = document.getElementById('id-show');
+const modalElem = document.getElementById('modal');
+const modalContentElem = document.getElementById('modal-content');
+const messageElem = document.getElementById('message');
+const modalResultElem = document.getElementById('modal-result');
+const modalContentResultElem = document.getElementById('modal-content-result');
+const resultElem = document.getElementById('result');
+
+//<-------------------------- Game Classes -------------------------->
+
+class HUD {
+  name;
+  id;
+  score;
+
+  constructor() {
+    this.score = 0;
+  }
+
+}
+
+class Sprite {
+  id;
+  positionX;
+  positionY;
+  color;
+  type;
+  points;
+  speed;
+
+  constructor(x, y, color, type, points, speed) {
+    this.id = createUUID();
+    this.positionX = x;
+    this.positionY = y;
+    this.color = color;
+    this.points = points;
+    this.speed = speed;
+    this.type = type;
+  }
+}
 
 class Game {
 
   config;
   backMusic;
-  name;
-  id;
-  score;
+  hud;
 
   intervalId;
   densityStep;
   balloonsArray;
 
-  canvasElement = document.getElementById('canvas');
-  playElement = document.getElementById('start-btn');
-  scoreElem = document.getElementById('score-count');
-  scoreTextElem = document.getElementById('score-text');
-  nameElem = document.getElementById('name-show');
-  idElem = document.getElementById('id-show');
-
-  constructor() {
-    const thiz = this;
-    this.updater = () => {
-      thiz.updateGame();
-    };
-  }
-
   startGame() {
-    this.playElement.style.display = 'none';
-    this.intervalId = setInterval(this.updater, updateTime);
-    this.backMusic = soundMap['background_music'];
-    this.backMusic.loop = true;
-    this.backMusic.volume = 0.3;
-    this.backMusic.play();
+    startPlayElement.style.display = 'none';
+    this.intervalId = setInterval(() => {
+      this.updateGame();
+    }, updateTime);
   }
 
   pauseLocalGame() {
@@ -132,8 +168,8 @@ class Game {
   }
 
   updateScore(score, type) {
-    this.scoreElem.innerHTML = score;
-    getPlayerRefById(this.id)
+    scoreLabelElem.innerHTML = score;
+    getPlayerRefById(this.hud.id)
       .update({
         score: score,
       })
@@ -146,61 +182,67 @@ class Game {
 
   }
 
-  buildBalloon(color, type, points) {
-    const tempBalloon = new Balloon(0, -balloonInitialHeight * this.config.balloonSize, color, type, points);
-    tempBalloon.positionX = generateRandomXPos(parseInt(this.canvasElement.style.width,10)*0.95);
+  buildSprite(color, type, points) {
+    const sprite = new Sprite(0, -balloonInitialHeight * this.config.balloonSize, color, type, points,
+      getRandomSpeed(this.config.baseSpeed));
+    sprite.positionX = generateRandomXPos(parseInt(canvasElement.style.width, 10) * 0.95);
 
     const el = document.createElement('div');
-    el.className = 'balloon ' + tempBalloon.color;
-    el.style.left = tempBalloon.positionX + 'px';
-    el.style.bottom = tempBalloon.positionY + 'px';
+    el.className = 'balloon ' + sprite.color;
+    el.style.left = sprite.positionX + 'px';
+    el.style.bottom = sprite.positionY + 'px';
     el.style.backgroundSize = '100% 100%';
     el.style.width = balloonInitialWidth * this.config.balloonSize + 'px';
     el.style.height = balloonInitialHeight * this.config.balloonSize + 'px';
 
-    const thiz = this;
+    const gameRef = this;
     el.onclick = () => {
-      if (!thiz.config.isPaused) {
-        thiz.score += points;
-        thiz.updateScore(thiz.score, type);
-        this.canvasElement.removeChild(el);
+      if (!gameRef.config.isPaused) {
+        gameRef.hud.score += points;
+        gameRef.updateScore(gameRef.hud.score, type);
+        canvasElement.removeChild(el);
       }
     };
 
-    this.canvasElement.appendChild(el);
+    canvasElement.appendChild(el);
     return {
       el: el,
-      speed: getRandomSpeed(this.config.baseSpeed),
-      points: tempBalloon.points,
+      speed: sprite.speed,
+      points: sprite.points,
       type: type,
+      bottom: sprite.positionY,
     };
   }
 
   applyConfig() {
+
+    if (this.config.hasFinished) {
+      this.endGame();
+    }
+
     if (!this.config.isPaused && !this.config.hasFinished && this.backMusic) {
       this.backMusic.play();
     }
 
-    if(this.config.showId){
-      this.idElem.style.display = 'block';
+    if (this.config.showId) {
+      idLabelElem.style.display = 'block';
     } else {
-      this.idElem.style.display = 'none';
+      idLabelElem.style.display = 'none';
     }
 
-    if(this.config.showName){
-      this.nameElem.style.display = 'block';
+    if (this.config.showName) {
+      nameLabelElem.style.display = 'block';
     } else {
-      this.nameElem.style.display = 'none';
+      nameLabelElem.style.display = 'none';
     }
 
-    if(this.config.showScore){
-      this.scoreElem.style.display = 'inline';
-      this.scoreTextElem.style.display = 'inline';
+    if (this.config.showScore) {
+      scoreLabelElem.style.display = 'inline';
+      scoreTextElem.style.display = 'inline';
     } else {
-      this.scoreElem.style.display = 'none';
-      this.scoreTextElem.style.display = 'none';
+      scoreLabelElem.style.display = 'none';
+      scoreTextElem.style.display = 'none';
     }
-
 
     this.balloonsArray.forEach((element) => {
       element.el.style.width = balloonInitialWidth * this.config.balloonSize + 'px';
@@ -209,55 +251,63 @@ class Game {
     });
   }
 
-  generateBalloons() {
+  generateSprites() {
     for (let i = 0; i < parseInt(this.densityStep, 10); i++) {
       let randomType = Math.floor(Math.random() * 100);
       let luckyFactor = getLuckyFactor();
       if (this.config.specialBalloon && (randomType % 25 === 0)) {
-        this.balloonsArray.push(this.buildBalloon('special', 'special', 300));
+        this.balloonsArray.push(this.buildSprite('special', 'special', 300));
       } else if (this.config.surpriseBalloon && (randomType % 20 === 0)) {
-        this.balloonsArray.push(this.buildBalloon('surprise', luckyFactor > 0 ? 'surprise_good' : 'surprise_bad', 400 * luckyFactor));
+        this.balloonsArray.push(this.buildSprite('surprise', luckyFactor > 0 ? 'surprise_good' : 'surprise_bad', 400 * luckyFactor));
       } else {
-        this.balloonsArray.push(this.buildBalloon('green', 'normal', 150));
+        this.balloonsArray.push(this.buildSprite('green', 'normal', 150));
       }
     }
+  }
+
+  moveSprites() {
+    this.balloonsArray.forEach((element) => {
+      const newPos = parseInt(element.el.style.bottom, 10) + (3 + element.speed);
+      element.el.style.bottom = newPos + 'px';
+      element.bottom = newPos;
+      if (element.type === 'special' && !this.config.specialBalloon) {
+        element.el.style.display = 'none';
+      } else if (element.type === 'special') {
+        element.el.style.display = 'block';
+      }
+
+      if (element.type.startsWith('surprise') && !this.config.surpriseBalloon) {
+        element.el.style.display = 'none';
+      } else if (element.type.startsWith('surprise')) {
+        element.el.style.display = 'block';
+      }
+    });
   }
 
   updateGame() {
     if (!this.config.isPaused && !this.config.hasFinished) {
       this.densityStep += this.config.density;
       if (this.densityStep >= 1 && this.balloonsArray.length < this.config.maxBalloonQuantity) {
-        this.generateBalloons();
+        this.generateSprites();
         this.densityStep = 0;
       }
+      this.moveSprites();
 
-      this.balloonsArray.forEach((element) => {
-        const newPos = parseInt(element.el.style.bottom, 10) + (3 + element.speed);
-        element.el.style.bottom = newPos + 'px';
-        if (element.type === 'special' && !this.config.specialBalloon) {
-          element.el.style.display = 'none';
-        } else if (element.type === 'special') {
-          element.el.style.display = 'block';
-        }
-
-        if (element.type.startsWith('surprise') && !this.config.surpriseBalloon) {
-          element.el.style.display = 'none';
-        } else if (element.type.startsWith('surprise')) {
-          element.el.style.display = 'block';
-        }
-      });
     } else if (this.config.isPaused && this.backMusic) {
       this.backMusic.pause();
     }
-    if (this.config.hasFinished) {
-      this.endGame();
+
+    if (this.balloonsArray.length === this.config.maxBalloonQuantity
+      && Math.min.apply(Math, this.balloonsArray.map(el => el.bottom)) >= canvasHeight + balloonInitialHeight * this.config.balloonSize) {
+      this.pauseLocalGame();
+      this.showResult(this.buildWaitMessage());
     }
   }
 
   buildGameOverMessage(firstPlayerDoc) {
     let gameOverText = 'GAME OVER\n\n';
     if (firstPlayerDoc.exists) {
-      if (firstPlayerDoc.data().id === this.id) {
+      if (firstPlayerDoc.data().id === this.hud.id) {
         gameOverText += 'I\'m the winner!\n ';
       }
       gameOverText += ('Congratulations ' + firstPlayerDoc.data().name + '!\n');
@@ -268,6 +318,25 @@ class Game {
     return gameOverText;
   }
 
+  buildWaitMessage() {
+    let gameOverText = 'GAME OVER\n\n At least for you, for now...\n\n';
+    gameOverText += ('Wait for the final result\n');
+    gameOverText += ('No worries, you did great!');
+    return gameOverText;
+  }
+
+  showResult(text) {
+    if (this.backMusic) {
+      this.backMusic.pause();
+    }
+
+    canvasElement.style.display = 'none';
+    modalResultElem.style.display = 'grid';
+    modalContentResultElem.style.display = 'grid';
+    resultElem.style.display = 'block';
+    resultElem.innerText = text;
+  }
+
   endGame() {
     this.pauseLocalGame();
     database.collection('players')
@@ -276,14 +345,7 @@ class Game {
       .get()
       .then((snapshot) => {
         snapshot.forEach((doc) => {
-          if (this.backMusic) {
-            this.backMusic.pause();
-          }
-          document.getElementById('canvas').style.display = 'none';
-          document.getElementById('modal-result').style.display = 'grid';
-          document.getElementById('modal-content-result').style.display = 'grid';
-          document.getElementById('result').style.display = 'block';
-          document.getElementById('result').innerText = this.buildGameOverMessage(doc);
+          this.showResult(this.buildGameOverMessage(doc));
         });
 
       })
@@ -294,37 +356,31 @@ class Game {
 
   initGame() {
     this.balloonsArray = [];
-    this.score = 0;
+    this.hud = new HUD();
     this.densityStep = 1;
+    canvasHeight = parseInt(canvasElement.style.height.replace('px', ''), 10);
 
+    this.backMusic = soundMap['background_music'];
+    this.backMusic.loop = true;
+    this.backMusic.volume = 0.3;
+    this.backMusic.play();
   }
 }
 
-class Balloon {
-  constructor(x, y, color, points) {
-    this.positionX = x;
-    this.positionY = y;
-    this.color = color;
-    this.points = points;
-  }
-}
+//<-------------------------- Env event setup and load -------------------------->
 
 function handleStartButtonClick(game) {
-  let name = document.getElementById('name').value + '';
+  let name = nameBoxElem.value + '';
   if (name && name.trim().length !== 0) {
-    document.getElementById('modal').style.display = 'none';
-    document.getElementById('modal-content').style.display = 'none';
-    document.getElementById('message').style.display = 'none';
-    game.id = createUUID();
-    game.name = name;
-    game.nameElem.innerHTML = game.name;
-    game.idElem.innerHTML = 'player id: ' + game.id;
+    game.hud.id = createUUID();
+    game.hud.name = name;
+
     database.collection('players')
-      .doc(game.id)
+      .doc(game.hud.id)
       .set({
-        name: game.name,
-        id: game.id,
-        score: 0,
+        name: game.hud.name,
+        id: game.hud.id,
+        score: game.hud.score,
       })
       .then(() => {
         game.startGame();
@@ -332,25 +388,31 @@ function handleStartButtonClick(game) {
       .catch((error) => {
         console.error('Error writing document: ', error);
       });
-  } else {
-    document.getElementById('message').style.display = 'block';
-  }
 
+    modalElem.style.display = 'none';
+    modalContentElem.style.display = 'none';
+    messageElem.style.display = 'none';
+    nameLabelElem.innerHTML = game.hud.name;
+    idLabelElem.innerHTML = 'player id: ' + game.hud.id;
+  } else {
+    messageElem.style.display = 'block';
+  }
 }
 
 function setupInitialEvents(game) {
-  document.getElementById('start-btn').onclick = () => {
+  startPlayElement.onclick = () => {
     handleStartButtonClick(game);
   };
 
-  document.getElementById('name')
-    .addEventListener('keyup', function (event) {
-      event.preventDefault();
-      if (event.key === 'Enter') {
-        handleStartButtonClick(game);
-      }
-    });
+  nameBoxElem.addEventListener('keyup', function (event) {
+    event.preventDefault();
+    if (event.key === 'Enter') {
+      handleStartButtonClick(game);
+    }
+  });
 }
+
+//<-------------------------- Main start -------------------------->
 
 window.addEventListener('load', () => {
   const game = new Game();
