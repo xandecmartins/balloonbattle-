6ebe1099bd68e0094b1cc099d4982a16a81d90ae
alexandre <alexandre.martins@skipthedishes.ca>
2019-12-23 12:36:05 -0600

    restart feature

--- public/game/js/game.js
+++ public/game/js/game.js
@@ -59,9 +59,12 @@ function generateRandomXPos(limit) {
 const spriteInitialWidth = 40;
 const spriteInitialHeight = 53;
 const updateTime = 50;
-let canvasHeight = 450;
+let canvasHeight;
+let canvasWidth;
 
-const canvasElement = document.getElementById('canvas');
+let canvasElement = document.getElementById('canvas');
+const canvasStyle = canvasElement.style;
+const canvasContainerElement = document.getElementById('canvas-container');
 const startPlayElement = document.getElementById('start-btn');
 const nameBoxElem = document.getElementById('name');
 const scoreLabelElem = document.getElementById('score-count');
@@ -75,302 +78,318 @@ const modalResultElem = document.getElementById('modal-result');
 const modalContentResultElem = document.getElementById('modal-content-result');
 const resultElem = document.getElementById('result');
 
-//<-------------------------- Game Classes -------------------------->
+//<-------------------------- Game Classes(Prototypes) -------------------------->
 
-class HUD {
-  name;
-  id;
-  score;
-
-  constructor() {
-    this.score = 0;
-  }
-
+function HUD() {
+  this.name;
+  this.id;
+  this.score = 0;
 }
 
-class Sprite {
-  id;
-  positionX;
-  positionY;
-  color;
-  type;
-  points;
-  speed;
-
-  constructor(x, y, color, type, points, speed) {
-    this.id = createUUID();
-    this.positionX = x;
-    this.positionY = y;
-    this.color = color;
-    this.points = points;
-    this.speed = speed;
-    this.type = type;
-  }
+function Sprite(x, y, color, type, points, speed) {
+  this.id = createUUID();
+  this.positionX = x;
+  this.positionY = y;
+  this.color = color;
+  this.points = points;
+  this.speed = speed;
+  this.type = type;
 }
 
-class Game {
+function Game() {
+  this.config;
+  this.backMusic;
+  this.hud;
+  this.intervalId;
+  this.densityStep;
+  this.spriteArray;
+  this.hasLocalFinished;
+}
 
-  config;
-  backMusic;
-  hud;
+Game.prototype.startGame = function () {
+  startPlayElement.style.display = 'none';
+  this.intervalId = setInterval(() => {
+    this.updateGame();
+  }, updateTime);
+};
 
-  intervalId;
-  densityStep;
-  spriteArray;
+Game.prototype.clearPlayArea = function () {
+  canvasElement.remove();
+};
 
-  startGame() {
-    startPlayElement.style.display = 'none';
-    this.intervalId = setInterval(() => {
-      this.updateGame();
-    }, updateTime);
-  }
+Game.prototype.restartGame = function () {
+  startPlayElement.style.display = 'block';
+  this.intervalId = setInterval(() => {
+    this.updateGame();
+  }, updateTime);
+  this.pauseLocalGame();
+  this.initGame();
 
-  pauseLocalGame() {
-    clearInterval(this.intervalId);
-  }
+  canvasElement = document.createElement('div');
+  canvasElement.setAttribute('id', 'canvas');
+  canvasElement.setAttribute('style.width', canvasStyle.width);
+  canvasContainerElement.appendChild(canvasElement);
+  modalResultElem.style.display = 'none';
+  modalContentResultElem.style.display = 'none';
+  modalElem.style.display = 'block';
+  modalContentElem.style.display = 'block';
+  canvasElement.style.display = 'block';
+};
 
-  loadServerConfig() {
-    getConfigServerRef()
-      .onSnapshot(doc => {
-        if (doc.exists) {
-          this.config = {
-            spriteSize: doc.data().balloon_size,
-            specialSprite: doc.data().special_balloon,
-            surpriseSprite: doc.data().surprise_balloon,
-            baseSpeed: doc.data().base_speed,
-            maxSpriteQuantity: doc.data().max_balloon_quantity,
-            isPaused: doc.data().is_paused,
-            hasFinished: doc.data().has_finished,
-            density: doc.data().density,
-            showName: doc.data().show_name,
-            showScore: doc.data().show_score,
-            showId: doc.data().show_id,
-          };
-          this.applyConfig();
-        } else {
-          this.config = {
-            spriteSize: 1,
-            specialSprite: true,
-            surpriseSprite: true,
-            baseSpeed: 201,
-            maxSpriteQuantity: 500,
-            isPaused: false,
-            hasFinished: false,
-            density: 1000 / 4000,
-            showName: true,
-            showScore: true,
-            showId: true,
-          };
-          console.log('Config doesn\'t exist on the server, using default values');
-        }
-      });
+Game.prototype.pauseLocalGame = function () {
+  clearInterval(this.intervalId);
+};
 
-  }
-
-  updateScore(score, type) {
-    scoreLabelElem.innerHTML = score;
-    getPlayerRefById(this.hud.id)
-      .update({
-        score: score,
-      })
-      .then(function () {
-        soundMap[type].play();
-      })
-      .catch(function (error) {
-        console.log('Data could not be saved.' + error);
-      });
-
-  }
-
-  buildSprite(color, type, points) {
-    const sprite = new Sprite(0, -spriteInitialHeight * this.config.spriteSize, color, type, points,
-      getRandomSpeed(this.config.baseSpeed));
-    sprite.positionX = generateRandomXPos(parseInt(canvasElement.style.width, 10) * 0.95);
-
-    const el = document.createElement('div');
-    el.className = 'sprite ' + sprite.color;
-    el.style.left = sprite.positionX + 'px';
-    el.style.bottom = sprite.positionY + 'px';
-    el.style.backgroundSize = '100% 100%';
-    el.style.width = spriteInitialWidth * this.config.spriteSize + 'px';
-    el.style.height = spriteInitialHeight * this.config.spriteSize + 'px';
-
-    const gameRef = this;
-    el.onclick = () => {
-      if (!gameRef.config.isPaused) {
-        gameRef.hud.score += points;
-        gameRef.updateScore(gameRef.hud.score, type);
-        canvasElement.removeChild(el);
-      }
-    };
-
-    canvasElement.appendChild(el);
-    return {
-      el: el,
-      speed: sprite.speed,
-      points: sprite.points,
-      type: type,
-      bottom: sprite.positionY,
-    };
-  }
-
-  applyConfig() {
-
-    if(this.config.hasFinished){
-      modalElem.style.display = 'none';
-      modalContentElem.style.display = 'none';
-      this.endGame();
-    } else {
-      modalResultElem.style.display = 'none';
-      modalContentResultElem.style.display = 'none';
-    }
-
-    if (!this.config.isPaused && !this.config.hasFinished && this.backMusic) {
-      this.backMusic.play();
-    }
-
-    if (this.config.showId) {
-      idLabelElem.style.display = 'block';
-    } else {
-      idLabelElem.style.display = 'none';
-    }
-
-    if (this.config.showName) {
-      nameLabelElem.style.display = 'block';
-    } else {
-      nameLabelElem.style.display = 'none';
-    }
-
-    if (this.config.showScore) {
-      scoreLabelElem.style.display = 'inline';
-      scoreTextElem.style.display = 'inline';
-    } else {
-      scoreLabelElem.style.display = 'none';
-      scoreTextElem.style.display = 'none';
-    }
-
-    this.spriteArray.forEach((element) => {
-      element.el.style.width = spriteInitialWidth * this.config.spriteSize + 'px';
-      element.el.style.height = spriteInitialHeight * this.config.spriteSize + 'px';
-      element.speed = getRandomSpeed(this.config.baseSpeed);
-    });
-  }
-
-  generateSprites() {
-    for (let i = 0; i < parseInt(this.densityStep, 10); i++) {
-      let randomType = Math.floor(Math.random() * 100);
-      if (this.config.specialSprite && (randomType % 20 === 0)) {
-        this.spriteArray.push(this.buildSprite('special', 'special', 300));
-      } else if (this.config.surpriseSprite && (randomType % 15 === 0)) {
-        let luckyFactor = getLuckyFactor();
-        this.spriteArray.push(this.buildSprite('surprise', luckyFactor > 0 ? 'surprise_good' : 'surprise_bad', 400 * luckyFactor));
+Game.prototype.loadServerConfig = function () {
+  getConfigServerRef()
+    .onSnapshot(doc => {
+      if (doc.exists) {
+        this.config = {
+          spriteSize: doc.data().balloon_size,
+          specialSprite: doc.data().special_balloon,
+          surpriseSprite: doc.data().surprise_balloon,
+          baseSpeed: doc.data().base_speed,
+          maxSpriteQuantity: doc.data().max_balloon_quantity,
+          isPaused: doc.data().is_paused,
+          hasFinished: doc.data().has_finished,
+          density: doc.data().density,
+          showName: doc.data().show_name,
+          showScore: doc.data().show_score,
+          showId: doc.data().show_id,
+        };
+        this.applyConfig();
       } else {
-        this.spriteArray.push(this.buildSprite('green', 'normal', 150));
-      }
-    }
-  }
-
-  moveSprites() {
-    this.spriteArray.forEach((element) => {
-      const newPos = parseInt(element.el.style.bottom, 10) + (3 + element.speed);
-      element.el.style.bottom = newPos + 'px';
-      element.bottom = newPos;
-      if (element.type === 'special' && !this.config.specialSprite) {
-        element.el.style.display = 'none';
-      } else if (element.type === 'special') {
-        element.el.style.display = 'block';
-      }
-
-      if (element.type.startsWith('surprise') && !this.config.surpriseSprite) {
-        element.el.style.display = 'none';
-      } else if (element.type.startsWith('surprise')) {
-        element.el.style.display = 'block';
+        this.config = {
+          spriteSize: 1,
+          specialSprite: true,
+          surpriseSprite: true,
+          baseSpeed: 201,
+          maxSpriteQuantity: 500,
+          isPaused: false,
+          hasFinished: false,
+          density: 1000 / 4000,
+          showName: true,
+          showScore: true,
+          showId: true,
+        };
+        console.log('Config doesn\'t exist on the server, using default values');
       }
     });
-  }
+};
 
-  updateGame() {
-    if (!this.config.isPaused && !this.config.hasFinished) {
-      this.densityStep += this.config.density;
-      if (this.densityStep >= 1 && this.spriteArray.length < this.config.maxSpriteQuantity) {
-        this.generateSprites();
-        this.densityStep = 0;
-      }
-      this.moveSprites();
+Game.prototype.updateScore = function (score, type) {
+  scoreLabelElem.innerHTML = score;
+  getPlayerRefById(this.hud.id)
+    .update({
+      score: score,
+    })
+    .then(function () {
+      soundMap[type].play();
+    })
+    .catch(function (error) {
+      console.log('Data could not be saved.' + error);
+    });
+};
 
-    } else if (this.config.isPaused && this.backMusic) {
-      this.backMusic.pause();
+Game.prototype.buildSprite = function (color, type, points) {
+  const sprite = new Sprite(0, -spriteInitialHeight * this.config.spriteSize, color, type, points,
+    getRandomSpeed(this.config.baseSpeed));
+  sprite.positionX = generateRandomXPos(canvasWidth * 0.95);
+
+  const el = document.createElement('div');
+  el.className = 'sprite ' + sprite.color;
+  el.style.left = sprite.positionX + 'px';
+  el.style.bottom = sprite.positionY + 'px';
+  el.style.backgroundSize = '100% 100%';
+  el.style.width = spriteInitialWidth * this.config.spriteSize + 'px';
+  el.style.height = spriteInitialHeight * this.config.spriteSize + 'px';
+
+  const gameRef = this;
+  el.onclick = () => {
+    if (!gameRef.config.isPaused) {
+      gameRef.hud.score += points;
+      gameRef.updateScore(gameRef.hud.score, type);
+      canvasElement.removeChild(el);
     }
+  };
 
-    if (this.spriteArray.length === this.config.maxSpriteQuantity
-      && Math.min.apply(Math, this.spriteArray.map(el => el.bottom)) >= canvasHeight + spriteInitialHeight * this.config.spriteSize) {
-      this.pauseLocalGame();
-      this.showResult(this.buildWaitMessage());
-    }
+  canvasElement.appendChild(el);
+  return {
+    el: el,
+    speed: sprite.speed,
+    points: sprite.points,
+    type: type,
+    bottom: sprite.positionY,
+  };
+};
+
+Game.prototype.applyConfig = function () {
+
+  if (this.config.hasFinished) {
+    modalElem.style.display = 'none';
+    modalContentElem.style.display = 'none';
+    this.endGame();
+  } else if (this.hasLocalFinished) {
+    this.restartGame();
   }
 
-  buildGameOverMessage(firstPlayerDoc) {
-    let gameOverText = 'GAME OVER\n\n';
-    if (firstPlayerDoc.exists) {
-      if (firstPlayerDoc.data().id === this.hud.id) {
-        gameOverText += 'I\'m the winner!\n ';
-      }
-      gameOverText += ('Congratulations ' + firstPlayerDoc.data().name + '!\n');
-      gameOverText += ('Winner! \n\n' + firstPlayerDoc.data().score + ' points');
-    } else {
-      gameOverText += 'Leader board is empty';
-    }
-    return gameOverText;
-  }
-
-  buildWaitMessage() {
-    let gameOverText = 'GAME OVER\n\n At least for you, for now...\n\n';
-    gameOverText += ('Wait for the final result\n');
-    gameOverText += ('No worries, you did great!');
-    return gameOverText;
-  }
-
-  showResult(text) {
-    if (this.backMusic) {
-      this.backMusic.pause();
-    }
-
-    canvasElement.style.display = 'none';
-    modalResultElem.style.display = 'block';
-    modalContentResultElem.style.display = 'block';
-    resultElem.style.display = 'block';
-    resultElem.innerText = text;
-  }
-
-  endGame() {
-    this.pauseLocalGame();
-    database.collection('players')
-      .orderBy('score', 'desc')
-      .limit(1)
-      .get()
-      .then((snapshot) => {
-        snapshot.forEach((doc) => {
-          this.showResult(this.buildGameOverMessage(doc));
-        });
-
-      })
-      .catch((error) => {
-        console.log('Error getting document:', error);
-      });
-  }
-
-  initGame() {
-    this.spriteArray = [];
-    this.hud = new HUD();
-    this.densityStep = 1;
-    canvasHeight = parseInt(canvasElement.style.height.replace('px', ''), 10);
-
-    this.backMusic = soundMap['background_music'];
-    this.backMusic.loop = true;
-    this.backMusic.volume = 0.3;
+  if (!this.config.isPaused && this.backMusic) {
     this.backMusic.play();
+  } else {
+    this.backMusic.pause();
   }
-}
+
+  if (this.config.showId) {
+    idLabelElem.style.display = 'block';
+  } else {
+    idLabelElem.style.display = 'none';
+  }
+
+  if (this.config.showName) {
+    nameLabelElem.style.display = 'block';
+  } else {
+    nameLabelElem.style.display = 'none';
+  }
+
+  if (this.config.showScore) {
+    scoreLabelElem.style.display = 'inline';
+    scoreTextElem.style.display = 'inline';
+  } else {
+    scoreLabelElem.style.display = 'none';
+    scoreTextElem.style.display = 'none';
+  }
+
+  this.spriteArray.forEach((element) => {
+    element.el.style.width = spriteInitialWidth * this.config.spriteSize + 'px';
+    element.el.style.height = spriteInitialHeight * this.config.spriteSize + 'px';
+    element.speed = getRandomSpeed(this.config.baseSpeed);
+  });
+};
+
+Game.prototype.generateSprites = function () {
+  for (let i = 0; i < parseInt(this.densityStep, 10); i++) {
+    let randomType = Math.floor(Math.random() * 100);
+    if (this.config.specialSprite && (randomType % 20 === 0)) {
+      this.spriteArray.push(this.buildSprite('special', 'special', 300));
+    } else if (this.config.surpriseSprite && (randomType % 15 === 0)) {
+      let luckyFactor = getLuckyFactor();
+      this.spriteArray.push(this.buildSprite('surprise', luckyFactor > 0 ? 'surprise_good' : 'surprise_bad', 400 * luckyFactor));
+    } else {
+      this.spriteArray.push(this.buildSprite('green', 'normal', 150));
+    }
+  }
+};
+
+Game.prototype.moveSprites = function () {
+  this.spriteArray.forEach((element) => {
+    const newPos = parseInt(element.el.style.bottom, 10) + (3 + element.speed);
+    element.el.style.bottom = newPos + 'px';
+    element.bottom = newPos;
+    if (element.type === 'special' && !this.config.specialSprite) {
+      element.el.style.display = 'none';
+    } else if (element.type === 'special') {
+      element.el.style.display = 'block';
+    }
+
+    if (element.type.startsWith('surprise') && !this.config.surpriseSprite) {
+      element.el.style.display = 'none';
+    } else if (element.type.startsWith('surprise')) {
+      element.el.style.display = 'block';
+    }
+  });
+};
+
+Game.prototype.updateGame = function () {
+  if (!this.config.isPaused && !this.config.hasFinished) {
+    this.densityStep += this.config.density;
+    if (this.densityStep >= 1 && this.spriteArray.length < this.config.maxSpriteQuantity) {
+      this.generateSprites();
+      this.densityStep = 0;
+    }
+    this.moveSprites();
+
+  } else if (this.config.isPaused && this.backMusic) {
+    this.backMusic.pause();
+  }
+
+  if (this.spriteArray.length === this.config.maxSpriteQuantity
+    && Math.min.apply(Math, this.spriteArray.map(el => el.bottom)) >= canvasHeight + spriteInitialHeight * this.config.spriteSize) {
+    this.pauseLocalGame();
+    this.showResult(this.buildWaitMessage());
+  }
+};
+
+Game.prototype.buildGameOverMessage = function (firstPlayerDoc) {
+  let gameOverText = 'GAME OVER\n\n';
+  if (firstPlayerDoc.exists) {
+    if (firstPlayerDoc.data().id === this.hud.id) {
+      gameOverText += 'I\'m the winner!\n ';
+    }
+    gameOverText += ('Congratulations ' + firstPlayerDoc.data().name + '!\n');
+    gameOverText += ('Winner! \n\n' + firstPlayerDoc.data().score + ' points');
+  } else {
+    gameOverText += 'Leader board is empty';
+  }
+  return gameOverText;
+};
+
+Game.prototype.buildWaitMessage = function () {
+  let gameOverText = 'GAME OVER\n\n At least for you, for now...\n\n';
+  gameOverText += ('Wait for the final result\n');
+  gameOverText += ('No worries, you did great!');
+  return gameOverText;
+};
+
+Game.prototype.showResult = function (text) {
+  if (this.backMusic) {
+    this.backMusic.pause();
+  }
+
+  canvasElement.style.display = 'none';
+  modalResultElem.style.display = 'block';
+  modalContentResultElem.style.display = 'block';
+  resultElem.style.display = 'block';
+  resultElem.innerText = text;
+};
+
+Game.prototype.endGame = function () {
+  this.backMusic.pause();
+  this.hasLocalFinished = true;
+  this.pauseLocalGame();
+  this.clearPlayArea();
+  database.collection('players')
+    .orderBy('score', 'desc')
+    .limit(1)
+    .get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        this.showResult(this.buildGameOverMessage(doc));
+      });
+
+    })
+    .catch((error) => {
+      console.log('Error getting document:', error);
+    });
+};
+
+Game.prototype.initGame = function () {
+  this.hasLocalFinished = false;
+  this.spriteArray = [];
+  this.hud = new HUD();
+  this.densityStep = 1;
+
+  if (!canvasHeight) {
+    canvasHeight = parseInt(canvasElement.style.height.replace('px', ''), 10);
+  }
+  if (!canvasWidth) {
+    canvasWidth = parseInt(canvasElement.style.height.replace('px', ''), 10);
+  }
+
+  this.backMusic = soundMap['background_music'];
+  this.backMusic.loop = true;
+  this.backMusic.volume = 0.3;
+  this.backMusic.play();
+};
 
 //<-------------------------- Env event setup and load -------------------------->
 
