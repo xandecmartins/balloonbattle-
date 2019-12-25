4e2a9bd9666041c315203bc5b334f6f24ebb9d78
alexandre <alexandre.martins@skipthedishes.ca>
2019-12-25 13:35:52 -0600

    new ranking code

--- public/ranking/js/ranking.js
+++ public/ranking/js/ranking.js
@@ -11,37 +11,75 @@ let firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-console.log('Initialisation Successful!');
-firebase.firestore()
-  .collection('players')
-  .orderBy('score', 'desc')
-  .onSnapshot(snapshot => {
-    const showList = document.getElementById('showList');
-    showList.innerHTML = '';
+let firstPlayerId;
+let lastPlayerId;
+const newLeaderAudio = new Audio('./sounds/leader.mp3');
+const newLanternAudio = new Audio('./sounds/lantern.mp3');
 
-    let html = '<table class="minimalistBlack"><thead><tr>';
-    html += '<th>Position</th>';
-    html += '<th>Id</th>';
-    html += '<th>Name</th>';
-    html += '<th>Score</th>';
-    /* add further columns into here, alike the one above. */
-    html += '</tr></thead><tbody>';
-    let pos = 1;
-    snapshot.forEach(doc => {
-      html += '<tr>';
+function updateRanking() {
 
-      html += '<td>' + pos + '</td>';
+  firebase.firestore()
+    .collection('players')
+    .where('score', '>', 0)
+    .orderBy('score', 'desc')
+    .onSnapshot(snapshot => {
+      const showList = document.getElementById('showList');
+      showList.innerHTML = '';
 
-      html += '<td>' + doc.data().id + '</td>';
+      let html = '<table class="minimalistBlack"><thead><tr>';
+      html += '<th>Position</th>';
+      html += '<th>Id</th>';
+      html += '<th>Name</th>';
+      html += '<th>Score</th>';
+      /* add further columns into here, alike the one above. */
+      html += '</tr></thead><tbody>';
+      let pos = 1;
 
-      html += '<td>' + doc.data().name + '</td>';
+      snapshot.forEach(doc => {
+        let iconTrophy = pos;
+        if (pos === 1) {
+          console.log(firstPlayerId);
+          if (firstPlayerId !== doc.data().id) {
+            firstPlayerId = doc.data().id;
+            newLeaderAudio.play()
+              .catch((error) => {
+                console.log('Problem during audio play', error);
+              });
+          }
+          iconTrophy = '<img class="trophy" src="./images/trophy.png"/>';
+          document.getElementById('nameFirst').innerText = doc.data().name;
+          document.getElementById('idFirst').innerText = doc.data().id;
+          document.getElementById('scoreFirst').innerText = doc.data().score;
+        } else if (pos === snapshot.size) {
 
-      html += '<td>' + doc.data().score + '</td>';
+          if (lastPlayerId !== doc.data().id) {
+            lastPlayerId = doc.data().id;
+          }
+          iconTrophy = '<img class="trophy" src="./images/lantern.png"/>';
+          document.getElementById('nameLast').innerText = doc.data().name;
+          document.getElementById('idLast').innerText = doc.data().id;
+          document.getElementById('scoreLast').innerText = doc.data().score;
+        }
 
-      html += '</tr>';
-      pos++;
+        html += '<tr>';
+
+        html += '<td class="column-table">' + iconTrophy + '</td>';
+
+        html += '<td class="column-table">' + doc.data().id + '</td>';
+
+        html += '<td class="column-table">' + doc.data().name + '</td>';
+
+        html += '<td class="column-score-table">' + doc.data().score + '</td>';
+
+        html += '</tr>';
+        pos++;
+      });
+      html += '</tbody></table>';
+
+      showList.insertAdjacentHTML('beforeend', html);
     });
-    html += '</tbody></table>';
+}
 
-    showList.insertAdjacentHTML('beforeend', html);
-  });
+window.addEventListener('load', () => {
+  updateRanking();
+});
